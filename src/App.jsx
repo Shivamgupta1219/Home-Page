8d71316f6258da37ac2b284354fa6e3d160644c8
shivam gupta <sgshivamgupta206@gmail.com>
2025-02-28 23:58:05 +0530

    Iniital Setup Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
-// import Navbbar from "./Navbbar";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      {/* <Navbbar />  */}
       <Hero />
       <Footer />
     </>
